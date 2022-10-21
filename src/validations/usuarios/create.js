fa71a325d40ad4b74b49cb092c4989d9c81032de
Diovane Camargo <diovane.cmelo@gmail.com>
2022-10-21 19:52:30 -0300

    Excluindo arquivos desnecessarios

--- src/validations/usuarios/create.js
+++
@@ -1,9 +0,0 @@
-const { validate, Joi } = require("express-validation");
-
-module.exports = validate({
-  body: Joi.object({
-    nome: Joi.string().required(),
-    email: Joi.string().email().required(),
-    senha: Joi.string().min(8).required(),
-  }),
-});
